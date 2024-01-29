b7124aa601714957086cb0ed91a22fbd5db15e79
sebascta <157431735+sebascta@users.noreply.github.com>
2024-01-29 19:30:19 -0300

    modulo1.py

--- modulo1.py
+++ modulo1.py
@@ -7,9 +7,17 @@ class Cliente:
         self.carrito = []
 
     def comprar(self, producto, tienda):
-        # Aquí podrías agregar lógica para procesar la compra, pero en este ejemplo simplemente imprimo un mensaje
+        # procesar la compra, imprimo un mensaje
         print(f"{self.nombre} ha comprado {producto} en {tienda}.")
 
     def __str__(self):
         return f"Cliente(nombre={self.nombre}, email={self.email}, edad={self.edad}, intereses={self.intereses})"
 
+# Instancia de Cliente1
+cliente1 = Cliente1("Matias", "Matias@gmail.com", 22, ["Deportes", "Tecnología"])
+
+# Realizar una compra
+cliente1.comprar("Celular", "claro")
+
+# Imprimir la información del cliente
+print(cliente1)
